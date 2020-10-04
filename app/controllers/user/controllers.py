5a31bc23ba52b24062acae7984d350afc0e19a37
juan.felipe <juan.felipe@aluno.faculdadeimpacta.com.br>
2020-10-04 20:32:38 -0300

    Adicionada proteção para que apenas o gerente tenha acesso ao cadastro de usuários, também adicionada guarda de rota

--- app/controllers/user/controllers.py
+++ app/controllers/user/controllers.py
@@ -1,5 +1,5 @@
 from flask import render_template, flash, redirect, url_for
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, current_user
 
 from . import user
 from app import app, db, login_manager
@@ -12,44 +12,51 @@ def get_user(login):
 
 @app.route('/register', methods=['GET','POST'])
 def register():
-    form = UserRegisterForm()
+    if current_user.is_authenticated and current_user.tipo == 'gerente':
+        form = UserRegisterForm()
 
-    if form.validate_on_submit():
-        #Obtem informações do formulário de registro
-        nome = form.nome.data
-        email = form.email.data
-        login = form.login.data
-        senha = form.senha.data
-        tipo =  form.tipo.data.lower()
+        if form.validate_on_submit():
+            #Obtem informações do formulário de registro
+            nome = form.nome.data
+            email = form.email.data
+            login = form.login.data
+            senha = form.senha.data
+            tipo =  form.tipo.data.lower()
 
-        #Cria objeto Usuario
-        usuario = Usuario(login=login, senha=senha, nome=nome, email=email, tipo=tipo)
+            #Cria objeto Usuario
+            usuario = Usuario(login=login, senha=senha, nome=nome, email=email, tipo=tipo)
 
-        #Grava no banco de dados
-        db.session.add(usuario)
-        db.session.commit()
+            #Grava no banco de dados
+            db.session.add(usuario)
+            db.session.commit()
 
-        return redirect('index')
-    return render_template('register_login.html', form=form)
+            return redirect('index')
+
+        return render_template('register_login.html', form=form)
+    
+    return redirect('index')
 
 
 @app.route('/login', methods=['GET','POST'])
 def login():
-    form = UserLoginForm()
+    if not current_user.is_authenticated:
+        form = UserLoginForm()
 
-    if form.validate_on_submit():
-        login = form.login.data
-        senha = form.senha.data
-        usuario = Usuario.query.filter_by(login=login).first()
+        if form.validate_on_submit():
+            login = form.login.data
+            senha = form.senha.data
+            usuario = Usuario.query.filter_by(login=login).first()
 
-        if usuario and usuario.verify_password(senha):
-            login_user(usuario)
-            return redirect('index')
-    return render_template('user_login.html', form=form)
+            if usuario and usuario.verify_password(senha):
+                login_user(usuario)
+                return redirect('index')
+
+        return render_template('user_login.html', form=form)
+
+    return redirect('index')
 
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect('login')
-
+    return redirect(url_for('login'))
 
