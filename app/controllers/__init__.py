97bbec6b10d297ebb88e7c4403bad8ce61370037
juan.felipe <juan.felipe@aluno.faculdadeimpacta.com.br>
2020-10-04 20:29:16 -0300

    Criado tratamento para páginas não existentes

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,5 +1,7 @@
 #módulo para registrar blueprints
 
+from flask import render_template
+
 from app import app
 
 from app.controllers.index import index as index_blueprint
@@ -7,3 +9,7 @@ app.register_blueprint(index_blueprint)
 
 from app.controllers.user import user as user_blueprint
 app.register_blueprint(user_blueprint)
+
+@app.errorhandler(404)
+def resource_not_found(e):
+    return render_template('not-found-page.html')
