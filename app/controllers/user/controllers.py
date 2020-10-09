375472b6c4290b9cf4784989644f77fdceefcf8c
juan.felipe <juan.felipe@aluno.faculdadeimpacta.com.br>
2020-10-09 20:44:40 -0300

    Ajustada alteração de usuário

--- app/controllers/user/controllers.py
+++ app/controllers/user/controllers.py
@@ -13,6 +13,7 @@ def get_user(login):
 
 @app.route('/cadastro-de-usuario', methods=['GET','POST'])
 def register():
+    #Guarda de rota, apena usuário autenticado e que for gerente pode registrar
     if current_user.is_authenticated and current_user.tipo.lower() == 'gerente':
         form = UserRegisterForm()
 
@@ -31,6 +32,7 @@ def register():
             db.session.add(usuario)
             db.session.commit()
 
+            #Redireciona para lista de usuários
             return redirect(url_for('list'))
 
         return render_template('user_register.html', form=form)
@@ -69,22 +71,33 @@ def list():
         return render_template('user_list.html', users=users)
     return redirect('pagina-inicial')
 
-@app.route('/editar-usuario/<string:login>', methods=['GET','PUT'])
+@app.route('/editar-usuario/<string:login>', methods=['GET','POST'])
 def edit(login):
     if current_user.is_authenticated and current_user.tipo.lower() == 'gerente':
         form = UserRegisterForm()
 
         print(form.validate_on_submit())
         if form.is_submitted():
-            #Obtem informações do formulário de registro
-            usuario = Usuario.query.filter_by(login=login).first()
-            usuario.nome = form.nome.data
-            usuario.email = form.email.data
-            usuario.senha = form.senha.data
-            usuario.tipo =  form.tipo.data.lower()
+            #Obtem usuário cadastrado no banco de dados
+            usuario_banco = Usuario.query.filter_by(login=login).first()
+            
+            #Informações do formulário
+            nome = form.nome.data
+            email = form.email.data
+            senha = form.senha.data
+            tipo =  form.tipo.data.lower()
+
+            #Monta objeto Usuario
+            usuario_model = Usuario(login=login, senha=senha, nome=nome, email=email, tipo=tipo)
+
+            #Altera informações para alteração no banco de dados
+            usuario_banco.nome = usuario_model.nome
+            usuario_banco.email = usuario_model.email
+            usuario_banco.senha = usuario_model.senha
+            usuario_banco.tipo = usuario_model.tipo
 
             #Grava no banco de dados
-            db.session.add(usuario)
+            db.session.add(usuario_banco)
             db.session.commit()
 
             return redirect(url_for('list'))
